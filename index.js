c4553c2650da27d866c2f22fc95a28236dc48a43
Wiku Hermawan <superwiku@gmail.com>
2025-08-29 20:58:55 +0700

    commit ketiga

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { GoogleGenAI } from '@google/genai';
+
+const  __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 
@@ -11,6 +17,7 @@ const GEMINI_MODEL = "gemini-2.5-flash";
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000;
 
